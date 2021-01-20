d9ccebe11c47ac567f53ddbedb1178383878d5fc
Beta-23 <alfred.dominguez@gmail.com>
2021-01-19 20:34:48 -0800

    Test event values from Form input

--- script.js
+++ script.js
@@ -2,8 +2,21 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
+
+let countdownTitle = '';
+let countdownDate = '';
+
 // Set Date Input Min & Value with Today's Date and ISOS date format
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+// Take values from Form input
+function updateCountdown(e) {
+    e.preventDefault();
+    console.log(e)
+}
+
+// Event Listener
+countdownForm.addEventListener('submit', updateCountdown);
 
 
