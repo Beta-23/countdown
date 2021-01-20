c2cc31b8343f0fde2e344142f73d1340c16b129a
Beta-23 <alfred.dominguez@gmail.com>
2021-01-19 16:26:59 -0800

    Set date input min & value with today's date

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
 // Set Date Input Min & Value with Today's Date and ISOS date format
-const today = new Date().toISOString();
-console.log(today);
+const today = new Date().toISOString().split('T')[0];
+dateEl.setAttribute('min', today);
 
 
