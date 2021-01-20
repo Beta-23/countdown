a614d77bda9d44b2b3f80520bad239a725946063
Beta-23 <alfred.dominguez@gmail.com>
2021-01-19 16:15:21 -0800

    Test the ISOS date format for date picker

---
+++ script.js
@@ -0,0 +1,9 @@
+const inputContainer = document.getElementById('input-container');
+const countdownForm = document.getElementById('countdownForm');
+const dateEl = document.getElementById('date-picker');
+
+// Set Date Input Min & Value with Today's Date and ISOS date format
+const today = new Date().toISOString();
+console.log(today);
+
+
