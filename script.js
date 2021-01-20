b38166f9d2ced6c13795b2358a120b2989f7c6e2
Beta-23 <alfred.dominguez@gmail.com>
2021-01-20 15:45:02 -0800

    Setup countdown and test populate countdown

--- script.js
+++ script.js
@@ -2,15 +2,36 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
+const countdownEl = document.getElementById('countdown');
+const countdownElTitle = document.getElementById('countdown-title');
+const countdownBtn = document.getElementById('countdown-button');
+const timeElements = document.querySelectorAll('span');
 
 let countdownTitle = '';
 let countdownDate = '';
 let countdownValue = Date;
 let savedCountdown;
 
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+
 // Set Date Input Min & Value with Today's Date and ISOS date format
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+// Populate Countdown / Complete UI
+function updateDOM() {
+  
+    const now = new Date().getTime();
+    const distance = countdownValue - now;
+    const days = Math.floor(distance / day);
+    const hours = Math.floor((distance % day) / hour);
+    const minutes = Math.floor((distance % hour) / minute);
+    const seconds = Math.floor((distance % minute) / second);
+    console.log(days, hours, minutes, seconds);
+}
 
 // Take values from Form input
 function updateCountdown(e) {
@@ -27,8 +48,9 @@ function updateCountdown(e) {
     if (countdownDate === '') {
         alert('Please select a date for the countdown.');
     } else {
-        // Get number version of current Date
+        // Get number version of current Date, updateDOM
         countdownValue = new Date(countdownDate).getTime();
+        updateDOM();
     } 
 }
 
